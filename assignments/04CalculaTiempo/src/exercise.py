d703417ca17dd63d6684086883399d60cbffe204
SofiaGuizar <A01641522@itesm.mx>
2021-08-22 05:50:39 +0000

    Tarea2

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
+    edadActual= int(input("Dame tu edad: "))
+    añoActual= int(input("Dame el año actual: "))
+    edadFutura= int(añoActual-edadActual)+100
+    print ("Cumplirás 100 años en el año: " +str(edadFutura))
     pass
 
 
--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
+    juegosNuevos= int(input("Dame la cantidad de juegos nuevos: "))
+    juegosUsados= int(input("Dame la cantidad de juegos usados: "))
+    precioJN= 1000
+    precioJU= 350
+    precioTotal= (precioJN*juegosNuevos)+(precioJU*juegosUsados)
+    print("El total de la compra es: " +str(precioTotal))
     pass
 
 
